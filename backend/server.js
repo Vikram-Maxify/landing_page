f58137d4027622faea6a89fcc0a92785310f7ff8
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-04-12 20:25:53 +0530

    by malik

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const path = require("path");
+
+app.use(cors());
+
+// API route
+
+// Static files
+app.use(express.static(path.join(__dirname, "../dist")));
+
+// React routing fix (IMPORTANT)
+app.get("", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
+
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
