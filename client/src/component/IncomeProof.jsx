df9915752ae462d1c83781e323e6df5c8802de8d
Vikram Thakur <trivikramkumar@maxifysolution.com>
2026-04-24 18:38:21 +0530

    incom proof

--- client/src/component/IncomeProof.jsx
+++ client/src/component/IncomeProof.jsx
@@ -1,63 +1,63 @@
 import React from "react";
 
 const IncomeProof = () => {
-  const data = [
-    {
-      url: "https://i.ibb.co/YBY4TNTp/Screenshot-2026-04-24-at-5-17-51-PM.png",
-      color: "green",
-      desc: "Commission from affiliate marketing",
-    },
-    {
-      url: "https://i.ibb.co/zLFX6pR/Screenshot-2026-04-24-at-5-17-01-PM.png",
-      color: "blue",
-      desc: "Social media handling & content",
-    },
-    {
-      url: "https://i.ibb.co/YBY4TNTp/Screenshot-2026-04-24-at-5-17-51-PM.png",
-      color: "purple",
-      desc: "Earnings from freelancing platforms",
-    },
-  ];
+    const data = [
+        {
+            url: "https://i.ibb.co/84rd4sYF/ssss.png",
+            color: "green",
+            desc: "Commission from affiliate marketing",
+        },
+        {
+            url: "https://i.ibb.co/zLFX6pR/Screenshot-2026-04-24-at-5-17-01-PM.png",
+            color: "blue",
+            desc: "Earning from secret platform ( Adstrox)",
+        },
+        {
+            url: "https://i.ibb.co/YBY4TNTp/Screenshot-2026-04-24-at-5-17-51-PM.png",
+            color: "purple",
+            desc: "Earning from secret platform ( Adstrox)",
+        },
+    ];
 
-  const colorStyles = {
-    green: "border-green-400 bg-green-50 text-green-700",
-    blue: "border-blue-400 bg-blue-50 text-blue-700",
-    purple: "border-purple-400 bg-purple-50 text-purple-700",
-  };
+    const colorStyles = {
+        green: "border-green-400 bg-green-50 text-green-700",
+        blue: "border-blue-400 bg-blue-50 text-blue-700",
+        purple: "border-purple-400 bg-purple-50 text-purple-700",
+    };
 
-  return (
-    <section className="px-6 md:px-16 py-10 bg-[#f6f8fc]">
-      <div className="text-center mb-4">
-        <h2 className="text-2xl md:text-4xl font-bold  text-gray-900">
-          Real Income Proof
-        </h2>
-        <p className="mt-2 text-gray-500">
-          Multiple Income Streams - Real Screenshots
-        </p>
-      </div>
+    return (
+        <section className="px-6 md:px-16 py-10 bg-[#f6f8fc]">
+            <div className="text-center mb-4">
+                <h2 className="text-2xl md:text-4xl font-bold  text-gray-900">
+                    Real Income Proof
+                </h2>
+                <p className="mt-2 text-gray-500">
+                    Multiple Income Streams - Real Screenshots
+                </p>
+            </div>
 
-      <div className="grid md:grid-cols-3 gap-6">
+            <div className="grid md:grid-cols-3 gap-6">
 
-        {data.map((item, i) => (
-          <div
-            key={i}
-            className={`p-3 rounded-2xl border ${colorStyles[item.color]}`}
-          >
-            <img
-              src={item.url}
-              alt="proof"
-              className="w- rounded-xl object-contain mb-3"
-            />
+                {data.map((item, i) => (
+                    <div
+                        key={i}
+                        className={`p-3 rounded-2xl border ${colorStyles[item.color]}`}
+                    >
+                        <img
+                            src={item.url}
+                            alt="proof"
+                            className="w- rounded-xl object-contain mb-3"
+                        />
 
-            <p className="text-sm font-medium text-center">
-              {item.desc}
-            </p>
-          </div>
-        ))}
+                        <p className="text-sm font-medium text-center">
+                            {item.desc}
+                        </p>
+                    </div>
+                ))}
 
-      </div>
-    </section>
-  );
+            </div>
+        </section>
+    );
 };
 
 export default IncomeProof;
